a5b67f0f5c04fa55cc3acdc03c617d9bf0ce7891
Alice Chou <alice1qaz@gmail.com>
2021-07-09 17:53:11 +0800

    add forex i18n

---
+++ src/i18n/localests/@types/forex.d.ts
@@ -0,0 +1,83 @@
+export type Forex = {
+  /********
+   * forex
+   ********/
+  /** Foreign exchange transactions */
+  foreignExchangeTransactions: string;
+  /** The foreign exchange */
+  theForeignExchange: string;
+  /** View trading products */
+  viewTradingProducts: string;
+  /** How to perform */
+  howToPerform: string;
+  /** Foreign exchange currencies */
+  foreignExchangeCurrencies: string;
+  /** In foreign exchange trading */
+  inForeignExchangeTrading: string;
+  /** Through WCG */
+  throughWCG: string;
+  /** Unlike other */
+  unlikeOther: string;
+  /** Buy-long */
+  buyLong: string;
+  /** Judge the market */
+  judgeTheMarket: string;
+  /** Sell-short */
+  sellShort: string;
+  /** Anticipating that */
+  anticipatingThat: string;
+  /** learn more */
+  learnMore: string;
+  /** Start forex trading */
+  startForexTrading: string;
+  /** Join a 24-hour */
+  joinA24Hour: string;
+  /** Choose a foreign */
+  chooseAForeign: string;
+  /** The first decision */
+  theFirstDecision: string;
+  /** In WCG */
+  inWCG: string;
+  /** Determine the direction */
+  determineTheDirection: string;
+  /** afterChoosing */
+  afterChoosing: string;
+  /** If you think that the base currency is stronger */
+  ifYouThinkThatTheBaseCurrencyIsStronger: string;
+  /** If you think that the base currency is weaker */
+  ifYouThinkThatTheBaseCurrencyIsWeaker: string;
+  /** At the same time */
+  atTheSameTime: string;
+  /** Set stop */
+  setStop: string;
+  /** Stop loss/take profit */
+  stopLossTakeProfit: string;
+  /** The purpose of  */
+  thePurposeOf: string;
+  /** Monitor your trading */
+  monitorYourTrading: string;
+  /** Before the position is closed */
+  beforeThePositionIsClosed: string;
+  /** So real-time monitoring */
+  soRealTimeMonitoring: string;
+  /** Close your position */
+  closeYourPosition: string;
+  /** It is extremely important */
+  itIsExtremelyImportant: string;
+  /** Failing to do so */
+  FailingToDoSo: string;
+  /** Foreign exchange quotes */
+  foreignExchangeQuotes: string;
+  /** Support leverage */
+  supportLeverage: string;
+  /** You can utilize leverage */
+  youCanUtilizeLeverage: string;
+  /** Funding threshold */
+  fundingThreshold: string;
+  /** When trading foreign exchange */
+  whenTradingForeignExchange: string;
+  /** 24 hours trading */
+  hoursTrading: string;
+  /** From Monday morning */
+  fromMondayMorning: string;
+};
--- src/i18n/localests/en/footer.ts
+++ src/i18n/localests/en/footer.ts
@@ -1,7 +1,7 @@
 import { Footer } from '../@types/footer';
 
 export const footer: Footer = {
-  copyright: '©*WCG*All*rights*reserved，*no*reprint',
+  copyright: '©*WCG*All*rights*reserved,*no*reprint',
   privacyPolicy: 'Privacy*Policy',
   termsAndConditions: 'Terms*and*Conditions',
   disputeSettlement: 'Dispute*disputeSettlement',
@@ -17,17 +17,17 @@ export const footer: Footer = {
   startRealFXTrading: 'Start*Real*FX*Trading',
   aRealAccount: 'a*real*account',
   WCGIsABusinessBrand:
-    'WCG*is*a*business*brand*jointly*shared*by*many*companies,*including：',
+    'WCG*is*a*business*brand*jointly*shared*by*many*companies,*including:',
   WCGMarketsLtdIsALimitedLiabilityCompany:
     'WCG*Markets*Ltd*is*a*limited*liability*company*registered*under*the*International*Business*Company*Law*in*Saint*Vincent*and*the*Grenadines.*The*company*number*is*26087*BC*2020.*The*registered*address*is:*The*Financial*Services*Centre*Stoney*Ground,*Kingstown,*St.Vincent*&*the*Grenadines.',
   WCGMarketsLtdIsRegulatedByTheFINTRAC:
     'WCG*Markets*Ltd*is*regulated*by*the*FINTRAC.*The*MSB*license*number*is*M20282836.*The*registered*address*is:*150-10451*Shellbridge*Way,*Richmond*BC*V6X*2W8,*Canada.',
   WenchuanGoldCompany:
-    'Wenchuan*Gold*Company*Limited*is*regulated*by*the*Hong*Kong*Gold*&*Silver*Exchange*(CGSE).*It*is*the*No.12*A1*member*unit.*The*registered*address*is:*Flat*D，*22/F，*Tower*B，*Capital*Tower，*38 Wai*Yip*Street，*Kowloon，*Hong*Kong.',
+    'Wenchuan*Gold*Company*Limited*is*regulated*by*the*Hong*Kong*Gold*&*Silver*Exchange*(CGSE).*It*is*the*No.12*A1*member*unit.*The*registered*address*is:*Flat*D,*22/F,*Tower*B,*Capital*Tower,*38 Wai*Yip*Street,*Kowloon,*Hong*Kong.',
   WenchuanGlobalLtd:
-    'Wenchuan*Global*Ltd*is*a*limited*liability*company*incorporated*in*the*United*Kingdom*with*the*company*number*11019948.*The*registered*address*is:*WC1E*6HA，*5th*Floor，*3*Gower*Street，*London，*UK.',
+    'Wenchuan*Global*Ltd*is*a*limited*liability*company*incorporated*in*the*United*Kingdom*with*the*company*number*11019948.*The*registered*address*is:*WC1E*6HA,*5th*Floor,*3*Gower*Street,*London,*UK.',
   HighRiskInvestmentWarning:
-    'High-risk*investment*warning:*Contracts*for*Difference*(CFD)*are*complex*financial*products，*and*their*use*of*leveraged*trading*attributes*is*likely*to*cause*rapid*loss*of*principal，*and*you*may*be*required*to*call*for*margin.*Please*understand*the*product*principles*of*CFDs*and*consider*whether*you*can*withstand*this*risk*before*entering*the*market.*The*past*prices*and*performance*of*all*financial*derivatives*instruments*do*not*guarantee*or*represent*future*trends.*Such*financial*products*are*not*suitable*for*all*investors.*Please*be*sure*to*fully*understand*all*potential*risks*before*entering*the*market*and*seek*independent*advice*when*necessary.',
+    'High-risk*investment*warning:*Contracts*for*Difference*(CFD)*are*complex*financial*products,*and*their*use*of*leveraged*trading*attributes*is*likely*to*cause*rapid*loss*of*principal,*and*you*may*be*required*to*call*for*margin.*Please*understand*the*product*principles*of*CFDs*and*consider*whether*you*can*withstand*this*risk*before*entering*the*market.*The*past*prices*and*performance*of*all*financial*derivatives*instruments*do*not*guarantee*or*represent*future*trends.*Such*financial*products*are*not*suitable*for*all*investors.*Please*be*sure*to*fully*understand*all*potential*risks*before*entering*the*market*and*seek*independent*advice*when*necessary.',
   restrictedAreas:
-    "Restricted*areas:*WCG*International*Co.，*Ltd.'s*website*information*does*not*aimed*at*targeting*residents*of*British*Columbia，*Quebec*and*Saskatchewan，*the*Democratic*People's*Republic*of*Korea*(North*Korea)，*Iran，*the*United*States*and*Hong*Kong;*And*not*to*send*or*use*the*information*to*people*in*countries*or*jurisdictions*where*the*publication*or*use*of this*information*violates*local*laws*and*regulations."
+    "Restricted*areas:*WCG*International*Co.,*Ltd.'s*website*information*does*not*aimed*at*targeting*residents*of*British*Columbia,*Quebec*and*Saskatchewan,*the*Democratic*People's*Republic*of*Korea*(North*Korea),*Iran,*the*United*States*and*Hong*Kong;*And*not*to*send*or*use*the*information*to*people*in*countries*or*jurisdictions*where*the*publication*or*use*of this*information*violates*local*laws*and*regulations."
 };
