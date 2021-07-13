4b862fa81249f3c0d21d67fb3edb410f45e29faf
Alice Chou <alice1qaz@gmail.com>
2021-07-13 16:03:54 +0800

    update logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,12 +1,10 @@
-import { Box, Image, Link } from '@chakra-ui/react';
+import { Image, Link } from '@chakra-ui/react';
 import React from 'react';
 
 const Logo: React.FC<{}> = () => {
   return (
     <Link href="/">
-      <Box>
-        <Image src="../assets/images/logo.png" alt="logo" />
-      </Box>
+      <Image maxW={'80%'} h="auto" src="../assets/images/logo.png" alt="logo" />
     </Link>
   );
 };
