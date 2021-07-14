45c15c3473294099fbd603c70968469b7e7de32b
Alice Chou <alice1qaz@gmail.com>
2021-07-14 12:12:59 +0800

    download pc page

--- src/components/Common/InfoCardSm.tsx
+++ src/components/Common/InfoCardSm.tsx
@@ -1,9 +1,26 @@
+import { Box, Center, Text } from '@chakra-ui/react';
 import React from 'react';
 
-interface InfoCardSmProps {}
+interface InfoCardSmProps {
+  icon?: JSX.Element;
+  image?: JSX.Element;
+  text: string;
+}
 
-const InfoCardSm: React.FC<InfoCardSmProps> = ({}) => {
-  return <div>InfoCardSm page!</div>;
+const InfoCardSm: React.FC<InfoCardSmProps> = ({
+  icon,
+  image,
+  text
+}: InfoCardSmProps) => {
+  return (
+    <Box p={10}>
+      <Box my={5} color="red.600">
+        {icon && <Center>{icon}</Center>}
+        {image && <Center>{image && image}</Center>}
+      </Box>
+      <Text>{text}</Text>
+    </Box>
+  );
 };
 
 export default InfoCardSm;
--- src/pages/platform/MT4PCDownload.tsx
+++ src/pages/platform/MT4PCDownload.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import React from 'react';
+import InfoCardSm from '../../components/Common/InfoCardSm';
 import InfoTitle from '../../components/Common/InfoTitle';
 import InfoTitleSub from '../../components/Common/InfoTitleSub';
 import DownloadButton from '../../components/Platform/DownloadButton';
@@ -57,6 +58,60 @@ const MT4PCDownload: React.FC<MT4PCDownloadProps> = () => {
         <Box width={{ base: '0', xl: '40vw' }}>
           <Image src="../assets/images/mt4pcdownload_1.png" alt="pc" />
         </Box>
+      </Flex>
+
+      <Flex py={20} align="center" direction="column">
+        <Box my={10} width={{ base: '80vw', md: '60vw' }} textAlign="center">
+          <InfoTitle title={t('comprehensiveTechnicalAnalysisFunction')} />
+          <InfoTitleSub title={t('professionalIconFunctions')} />
+        </Box>
+        <Flex wrap="wrap" justify="center">
+          <InfoCardSm
+            image={
+              <Image
+                src="../assets/images/mt4pcdownload_icon1.png"
+                alt="icon1"
+              />
+            }
+            text={t('automaticChartingRecords')}
+          />
+          <InfoCardSm
+            image={
+              <Image
+                src="../assets/images/mt4pcdownload_icon2.png"
+                alt="icon2"
+              />
+            }
+            text={t('selfDefinedTechnicalIndicators')}
+          />
+          <InfoCardSm
+            image={
+              <Image
+                src="../assets/images/mt4pcdownload_icon3.png"
+                alt="icon3"
+              />
+            }
+            text={t('supportMultipleWindows')}
+          />
+          <InfoCardSm
+            image={
+              <Image
+                src="../assets/images/mt4pcdownload_icon4.png"
+                alt="icon4"
+              />
+            }
+            text={t('simpleChartingOperation')}
+          />
+          <InfoCardSm
+            image={
+              <Image
+                src="../assets/images/mt4pcdownload_icon5.png"
+                alt="icon5"
+              />
+            }
+            text={t('nineChartingTimePeriods')}
+          />
+        </Flex>
       </Flex>
     </Wrapper>
   );
