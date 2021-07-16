222ab9d5f5f55b00cb9b9b54a726c6c4a31369eb
Alice Chou <alice1qaz@gmail.com>
2021-07-16 16:00:08 +0800

    add preciousMetals i18n

---
+++ src/i18n/localests/@types/preciousMetals.d.ts
@@ -0,0 +1,71 @@
+export type PreciousMetals = {
+  /********
+   * preciousMetals
+   ********/
+  /** How to trade precious metals */
+  howToTradePreciousMetals: string;
+  /** For those traders */
+  forThoseTraders: string;
+  /** This means that when trading gold */
+  thisMeansThatWhenTradingGold: string;
+  /** When the price rises to */
+  whenThePriceRisesTo: string;
+  /** Diversified investment */
+  diversifiedInvestment: string;
+  /** Just as experienced investors */
+  justAsExperiencedInvestors: string;
+  /** Fight against inflation */
+  fightAgainstInflation: string;
+  /** As inflation intensifies */
+  asInflationIntensifies: string;
+  /** High storage value */
+  highStorageValue: string;
+  /** About 95% of the world's gold */
+  about95OfTheWorldsGold: string;
+  /** Precious metals are difficult to mine */
+  preciousMetalsAreDifficultToMine: string;
+  /** Among all precious metals */
+  amongAllPreciousMetals: string;
+  /** Participants in gold trading */
+  participantsInGoldTrading: string;
+  /** The gold market attracts */
+  theGoldMarketAttracts: string;
+  /** Gold believers */
+  goldBelievers: string;
+  /** Mainly include individual investors */
+  mainlyIncludeIndividualInvestors: string;
+  /** Large institutions */
+  largeInstitutions: string;
+  /** Such institutions include hedge funds */
+  suchInstitutionsIncludeHedgeFunds: string;
+  /** Reasons affecting the price of gold */
+  reasonsAffectingThePriceOfGold: string;
+  /** There are many factors */
+  thereAreManyFactors: string;
+  /** Central Bank */
+  centralBank: string;
+  /** These institutions */
+  theseInstitutions: string;
+  /** Crude oil */
+  crudeOil: string;
+  /** Crude oil and gold */
+  crudeOilAndGold: string;
+  /** U.S. Dollars */
+  USDollars: string;
+  /** Since gold is priced */
+  sinceGoldIsPriced: string;
+  /** Stock Market */
+  stockMarket: string;
+  /** When the stock market */
+  whenTheStockMarket: string;
+  /** Historical gold price */
+  historicalGoldPrice: string;
+  /** The following table */
+  theFollowingTable: string;
+  /** During this period */
+  duringThisPeriod: string;
+  /** On the other hand */
+  onTheOtherHand: string;
+  /** View trading products */
+  viewTradingProducts: string;
+};
