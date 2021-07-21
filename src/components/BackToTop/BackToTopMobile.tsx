1c18444081cfd469361366d3fe06cf341dc44c06
Alice Chou <alice1qaz@gmail.com>
2021-07-21 17:38:21 +0800

    temp BackToTopMobile

--- src/components/BackToTop/BackToTopMobile.tsx
+++ src/components/BackToTop/BackToTopMobile.tsx
@@ -1,124 +1,175 @@
-import { Box, Flex, IconButton } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
-import { AiOutlineCloudDownload, AiOutlineWhatsApp } from 'react-icons/ai';
+import {
+  AiOutlineCloudDownload,
+  AiOutlineMessage,
+  AiOutlineWhatsApp
+} from 'react-icons/ai';
 import { BiArrowToTop } from 'react-icons/bi';
 import { BsPencilSquare } from 'react-icons/bs';
 import { FaLine, FaTelegramPlane } from 'react-icons/fa';
+import { GiTalk } from 'react-icons/gi';
 import { Locales } from '../../i18n/locales';
-import { scrollToTop } from '../../utils';
+import { openChatWindow, scrollToTop } from '../../utils';
 
 const BackToTopMobile: React.FC<{}> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { t } = useTranslation(['common']);
   const router = useRouter();
   const currentLang = router.locale as Locales;
 
+  const handleClose = () => {
+    onClose;
+    openChatWindow();
+  };
+
   return (
-    <Box zIndex={1} position="fixed" bottom={1} left={1}>
-      <Flex direction={{ base: 'row', md: 'column' }} color="white">
+    <>
+      <Box position="fixed" zIndex={1} bottom="0" right="0">
         <IconButton
-          as="a"
-          href="https://api.whatsapp.com/message/2CCAPQTYUPV6P1"
-          target="_blank"
-          ml={3}
-          borderRadius="50%"
-          aria-label={t('whatsApp')}
-          aria-hidden
-          icon={<AiOutlineWhatsApp fontSize="32px" />}
-          bgColor="gray.600"
-          borderColor="white"
-          border="1px"
+          size="lg"
+          bg="gray.600"
           _hover={{
             bgColor: 'gray.600'
           }}
-        />
-
-        <IconButton
-          as="a"
-          href="https://page.line.me/?accountId=739zqmre&openerPlatform=native&openerKey=talkroom%3Amessage"
-          target="_blank"
-          ml={3}
-          borderRadius="50%"
-          aria-label={t('line')}
-          aria-hidden
-          icon={<FaLine fontSize="32px" />}
-          bgColor="gray.600"
-          borderColor="white"
-          border="1px"
-          _hover={{
-            bgColor: 'gray.600'
-          }}
-        />
-
-        <IconButton
-          as="a"
-          href="https://t.me/WCGMarketsLtd"
-          target="_blank"
-          ml={3}
-          borderRadius="50%"
-          aria-label={t('telegram')}
-          aria-hidden
-          icon={<FaTelegramPlane fontSize="32px" />}
-          bgColor="gray.600"
-          borderColor="white"
-          border="1px"
-          _hover={{
-            bgColor: 'gray.600'
-          }}
-        />
-
-        <IconButton
-          as="a"
-          href="https://trader.wc012.com/register"
-          target="_blank"
-          ml={3}
-          borderRadius="50%"
-          aria-label={t('clientPortal')}
-          aria-hidden
-          icon={<BsPencilSquare fontSize="32px" />}
-          bgColor="gray.600"
-          borderColor="white"
-          border="1px"
-          _hover={{
-            bgColor: 'gray.600'
-          }}
-        />
-
-        <NextLink href="/platform/MT4Overview" locale={currentLang}>
-          <IconButton
-            as="a"
-            ml={3}
-            borderRadius="50%"
-            aria-label={t('downloadMT4')}
-            aria-hidden
-            icon={<AiOutlineCloudDownload fontSize="32px" />}
-            bgColor="gray.600"
-            borderColor="white"
-            border="1px"
-            _hover={{
-              bgColor: 'gray.600',
-              cursor: 'pointer'
-            }}
-          />
-        </NextLink>
-
-        <IconButton
-          ml={3}
-          borderRadius="50%"
+          color="white"
+          title={t('top')}
           onClick={scrollToTop}
           aria-label={t('top')}
-          icon={<BiArrowToTop fontSize="32px" />}
-          bgColor="gray.600"
-          borderColor="white"
-          border="1px"
+          icon={<BiArrowToTop fontSize="40px" />}
+        />
+      </Box>
+
+      <Box position="fixed" zIndex={1} bottom="0" left="0">
+        <IconButton
+          size="lg"
+          bg="gray.600"
           _hover={{
             bgColor: 'gray.600'
           }}
+          color="white"
+          title={t('contactUs')}
+          onClick={onOpen}
+          aria-label={t('contactUs')}
+          icon={<GiTalk fontSize="40px" />}
         />
-      </Flex>
-    </Box>
+      </Box>
+
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        isCentered
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center">{t('contactUs')}</ModalHeader>
+          <ModalCloseButton size="lg" />
+          <ModalBody pb={6}>
+            <Flex color="white" justify="space-around">
+              <IconButton
+                size="lg"
+                onClick={handleClose}
+                borderRadius="50%"
+                aria-label={t('online')}
+                aria-hidden
+                icon={<AiOutlineMessage fontSize="36px" />}
+                bg="gray.600"
+                _hover={{
+                  bgColor: 'gray.600'
+                }}
+              />
+              <IconButton
+                size="lg"
+                as="a"
+                href="https://api.whatsapp.com/message/2CCAPQTYUPV6P1"
+                target="_blank"
+                borderRadius="50%"
+                aria-label={t('whatsApp')}
+                aria-hidden
+                icon={<AiOutlineWhatsApp fontSize="36px" />}
+                bg="whatsapp.600"
+                _hover={{
+                  bgColor: 'whatsapp.600'
+                }}
+              />
+
+              <IconButton
+                size="lg"
+                as="a"
+                href="https://t.me/WCGMarketsLtd"
+                target="_blank"
+                borderRadius="50%"
+                aria-label={t('telegram')}
+                aria-hidden
+                icon={<FaTelegramPlane fontSize="36px" />}
+                bg="twitter.500"
+                _hover={{
+                  bgColor: 'twitter.500'
+                }}
+              />
+
+              <IconButton
+                size="lg"
+                as="a"
+                href="https://page.line.me/?accountId=739zqmre&openerPlatform=native&openerKey=talkroom%3Amessage"
+                target="_blank"
+                borderRadius="50%"
+                aria-label={t('line')}
+                aria-hidden
+                icon={<FaLine fontSize="36px" />}
+                bg="whatsapp.600"
+                _hover={{
+                  bgColor: 'whatsapp.600'
+                }}
+              />
+              <IconButton
+                size="lg"
+                as="a"
+                href="https://trader.wc012.com/register"
+                target="_blank"
+                borderRadius="50%"
+                aria-label={t('clientPortal')}
+                aria-hidden
+                icon={<BsPencilSquare fontSize="36px" />}
+                bg="purple.600"
+                _hover={{
+                  bgColor: 'purple.600'
+                }}
+              />
+              <NextLink href="/platform/MT4Overview" locale={currentLang}>
+                <IconButton
+                  size="lg"
+                  as="a"
+                  borderRadius="50%"
+                  aria-label={t('downloadMT4')}
+                  aria-hidden
+                  icon={<AiOutlineCloudDownload fontSize="36px" />}
+                  bg="red.600"
+                  _hover={{
+                    bgColor: 'red.600'
+                  }}
+                />
+              </NextLink>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
