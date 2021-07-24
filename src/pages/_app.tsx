8833e390ecfa97eac218a7002456b5ef73724598
Alice Chou <alice1qaz@gmail.com>
2021-07-24 17:11:51 +0800

    style(index.css): add scroll-behavior: "smooth"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 import Router from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
+import '../styles/index.css'; // global styling
 import '../styles/nprogress.css'; //styles of nprogress
 import theme from '../styles/theme';
 
