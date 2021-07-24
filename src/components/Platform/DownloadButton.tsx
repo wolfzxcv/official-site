bdfee572246713148e2c74eeed301a30095e2833
Alice Chou <alice1qaz@gmail.com>
2021-07-24 21:30:31 +0800

    refactor(downloadbutton): forwardRef

--- src/components/Platform/DownloadButton.tsx
+++ src/components/Platform/DownloadButton.tsx
@@ -1,6 +1,6 @@
 import { Box, Center, Image, Link, Tooltip } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { BiUser } from 'react-icons/bi';
 import { Locales } from '../../i18n/locales';
@@ -19,10 +19,6 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
   type,
   onClick
 }: DownloadButtonProps) => {
-  const router = useRouter();
-  const currentLang = router.locale as Locales;
-  const isChinese = currentLang === 'cn' || currentLang === 'zh';
-  const isArabic = currentLang === 'ar';
   return (
     <>
       {href ? (
@@ -56,44 +52,7 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
             hasArrow
             arrowSize={15}
           >
-            <StyledFlex
-              onClick={onClick ? onClick : () => false}
-              direction={isArabic ? 'row-reverse' : 'row'}
-              justify="center"
-              p={3}
-              fontSize={isChinese ? '28px' : '22px'}
-              bg="#4da506"
-              color="white"
-              _hover={{
-                bgColor: 'green.600',
-                transform: 'scale(1.1)',
-                cursor: 'pointer'
-              }}
-            >
-              <Center>
-                {type === 'android' ? (
-                  <Image
-                    minW="28px"
-                    height="28px"
-                    src="../assets/images/android.png"
-                    alt="android"
-                  />
-                ) : type === 'ios' ? (
-                  <Image
-                    minW="28px"
-                    height="28px"
-                    src="../assets/images/ios.png"
-                    alt="ios"
-                  />
-                ) : type === 'user' ? (
-                  <BiUser fontSize="36px" />
-                ) : (
-                  <AiOutlineDownload fontSize="40px" />
-                )}
-              </Center>
-
-              <Box mx={2}>{text}</Box>
-            </StyledFlex>
+            <CommonContent text={text} type={type} onClick={onClick} />
           </Tooltip>
         </Link>
       ) : (
@@ -103,55 +62,57 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
   );
 };
 
-const CommonContent: React.FC<DownloadButtonProps> = ({
-  text,
-  type,
-  onClick
-}: DownloadButtonProps) => {
-  const router = useRouter();
-  const currentLang = router.locale as Locales;
-  const isChinese = currentLang === 'cn' || currentLang === 'zh';
-  const isArabic = currentLang === 'ar';
-  return (
-    <StyledFlex
-      onClick={onClick ? onClick : () => false}
-      direction={isArabic ? 'row-reverse' : 'row'}
-      justify="center"
-      p={3}
-      fontSize={isChinese ? '28px' : '22px'}
-      bg="#4da506"
-      color="white"
-      _hover={{
-        bgColor: 'green.600',
-        transform: 'scale(1.1)',
-        cursor: 'pointer'
-      }}
-    >
-      <Center>
-        {type === 'android' ? (
-          <Image
-            minW="28px"
-            height="28px"
-            src="../assets/images/android.png"
-            alt="android"
-          />
-        ) : type === 'ios' ? (
-          <Image
-            minW="28px"
-            height="28px"
-            src="../assets/images/ios.png"
-            alt="ios"
-          />
-        ) : type === 'user' ? (
-          <BiUser fontSize="36px" />
-        ) : (
-          <AiOutlineDownload fontSize="40px" />
-        )}
-      </Center>
+const CommonContent = forwardRef<HTMLDivElement, DownloadButtonProps>(
+  ({ text, type, onClick, ...rest }: DownloadButtonProps, ref) => {
+    const router = useRouter();
+    const currentLang = router.locale as Locales;
+    const isChinese = currentLang === 'cn' || currentLang === 'zh';
+    const isArabic = currentLang === 'ar';
+    return (
+      <StyledFlex
+        ref={ref}
+        {...rest}
+        onClick={onClick ? onClick : () => false}
+        direction={isArabic ? 'row-reverse' : 'row'}
+        justify="center"
+        p={3}
+        fontSize={isChinese ? '28px' : '22px'}
+        bg="#4da506"
+        color="white"
+        _hover={{
+          bgColor: 'green.600',
+          transform: 'scale(1.1)',
+          cursor: 'pointer'
+        }}
+      >
+        <Center>
+          {type === 'android' ? (
+            <Image
+              minW="28px"
+              height="28px"
+              src="../assets/images/android.png"
+              alt="android"
+            />
+          ) : type === 'ios' ? (
+            <Image
+              minW="28px"
+              height="28px"
+              src="../assets/images/ios.png"
+              alt="ios"
+            />
+          ) : type === 'user' ? (
+            <BiUser fontSize="36px" />
+          ) : (
+            <AiOutlineDownload fontSize="40px" />
+          )}
+        </Center>
 
-      <Box mx={2}>{text}</Box>
-    </StyledFlex>
-  );
-};
+        <Box mx={2}>{text}</Box>
+      </StyledFlex>
+    );
+  }
+);
+
+CommonContent.displayName = 'CommonContent';
 
 export default DownloadButton;
