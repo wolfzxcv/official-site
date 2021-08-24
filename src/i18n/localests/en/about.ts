6955522c4b2d533176e84e8ac00ccf44818d8dd4
Alice Chou <alice1qaz@gmail.com>
2021-08-24 17:08:50 +0800

    add i18n about

---
+++ src/i18n/localests/en/about.ts
@@ -0,0 +1,59 @@
+import { About } from '../@types/about';
+
+export const about: About = {
+  buildTheWorldWithIntegrity:
+    'Build*the*world*with*integrity,*win*the*future*steadily',
+  WCGHasASolidFoundation:
+    "WCG*has*a*solid*foundation*and*focusing*on*continuous*innovation.*So*far,*the**industry's*best*user*experience*and*professional*services*have*enabled*us*to*continue*to*be*a*market*leader*and*continue*to*be*committed*to*providing*the*best*trading*environment*for*global*investors.",
+  registerARealAccountNow: 'Register*a*real*account*now',
+  registerADemoAccountNow: 'Register*a*demo*account*now',
+  strictlyRegulatedAndTrustworthy: 'Strictly*regulated*and*trustworthy',
+  WCGInternational:
+    'WCG*International*is*a*professional*financial*investment*brand*established*in*Saint*Vincent*and*the*Grenadines,*subject*to*strict*laws*and*regulations,*and*registered*in*accordance*with*the*International*Business*Company*Law*(BCN:*25293*BC*2019),*They*have*strict*regulations*to*protect*the*interests*of*investors.',
+  tradingProducts: 'Trading*products',
+  tradingPlatform: 'Trading*platform',
+  platformHasWonManyAwards: 'Platform*has*won*many*awards',
+  basedOnTheBasicPrinciples:
+    "Based*on*the*basic*principles*of*fairness,*justice*and*transparency,*it*has*been*trusted*and*supported*by*users*all*over*the*world.*Company's*goal*is*to*be*customer-centric*and*sincere",
+  multiPartySystemSupport: 'Multi-party*system*support',
+  fullSupport:
+    "Full*support*for*personal*computers,*mobile*phones,*tablets*and*other*systems*to*satisfy*investors'*needs*without*leaving*home,*real-time*access*to*currency*quotations*and*financial*product*information.",
+  rapidTransaction: 'Rapid*transaction',
+  globallyRecognizedLiquidity:
+    'Globally*recognized*liquidity*providers*with*significant*market*depth*and*low-latency*connections.*So*that*our*customers*can*obtain*fast*execution*speed*with*low*latency',
+  excellentCustomerService: 'Excellent*customer*service',
+  provideServices:
+    'Provide*services*such*as*excellent*execution,*transparent*quotation,*high-quality*tools,*market*news*and*professional*knowledge,*so*that*users*can*get*more*potential*benefits.',
+  diversifiedProducts: 'Diversified*products,*create*the*future',
+  WCGProvides24Hours:
+    'WCG*provides*24-hours*online*global*financial*product*trading*services*for*foreign*exchange,*index,*energy,*commodity,*cypto*currency*and*other*CFDs*for*global*customers.*Through*highly*competitive*products,*excellent*leading*technology*and*professional*and*considerate*services,*we*provide*customers*with*top-level*trading*and*investment*experience*and*achieve*the*goal*of*wealth*appreciation.',
+  WCGIsAlwaysYourWiseChoice: 'WCG*is*always*your*wise*choice',
+  WCGKeepsAdvancingWithTimes:
+    "WCG*keeps*advancing*with*times.*For*this*reason,*we*insist*on*investing*in*the*latest*technology,*which*not*only*makes*the*account*opening*and*review*process*faster,*but*also*provides*a*convenient*payment*system*for*deposit*and*withdrawal*purposes.*We*use*the*latest*bridging*technology*to*connect*MT4*and*top*liquidity*providers,*so*that*customers*can*trade*while*traveling*and*immediately*respond*to*market*activity*news*and*data.*In*order*to*adhere*to*our*core*principle*of*'fairness*and*justice',*we*ensure*that*all*customer*transactions*are*automatic*executed.*At*the*same*time,*we*also*pay*much*attention*to*customer's*trading*experience.*The*large*expert*team*of*WCG*will*provide*you*with*one-to-one*customer*service*at*any*time,*so*that*you*can*enjoy*the*world's*best*trading*environment.",
+  goToThePlatformToDownload: 'Go*to*the*platform*to*download',
+  yearsOfCredibility: 'Years*of*credibility',
+  adheringToTheOriginalIntention:
+    'Adhering*to*the*original*intention*of*safety*standards,*integrity*and*reliability,*continuous*innovation,*continuous*development,*strict*compliance*with*regulations,*and*strive*to*achieve*the*highest*standards*of*industry*credibility.',
+  abundantResources: 'Abundant*resources',
+  fullyProvideCustomers:
+    'Fully*provide*customers*with*a*safe*operating*system,*complete*trading*tools,*abundant*financial*resources*and*professional*global*support.',
+  qualityService: 'Quality*service',
+  ourSalesTeam:
+    'Our*sales*team*and*customer*support*team*have*received*professional*training,*and*strive*to*provide*you*with*the*best*trading*experience.',
+  becomeOurPartner: 'Become*our*partner',
+  joinWCGsPartnerProgram:
+    "Join*WCG's*partner*program*and*start*your*own*business*immediately!",
+  joinNow: 'Join*now',
+  mrXiao: 'Mr.*Xiao',
+  inTheBeginning:
+    "In*the*beginning,*I*just*immersed*myself*to*work*in*a*small*rented*office,*and*my*first*profit*just*offset*the*initial*investment.*Today,*I*am*one*of*WCG's*most*successful*partners.",
+  msHuang: 'Ms.*Huang',
+  becauseIAmGood:
+    'Because*I*am*good*at*running*communities*on*social*networks.*Through*word-of-mouth*marketing,*the*number*of*customers*continues*to*grow*and*profits*continue*to*increase.*Thank*you*WCG*for*giving*us*such*a*great*trading*platform.',
+  msZheng: 'Ms.*Zheng',
+  whenIFirstCameInto:
+    'When*I*first*came*into*contact*with*WenChuan,*I*fell*in*love*with*the*simple*yet*comprehensive*operation*mode.*When*I*encountered*a*problem,*the*customer*service*was*very*enthusiastic*and*willing*to*help*out,*and*my*customers*were*very*satisfied.',
+  msLai: 'Ms.*Lai',
+  whileWorkingWithWCG:
+    'While*working*with*WCG,*I*learned*a*lot*about*the*financial*market.*Now*I*often*share*market*views*and*resources*with*my*friends.'
+};
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,18 @@
+import { Box, Flex } from '@chakra-ui/react';
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
 import Wrapper from '../../components/Wrapper';
 
-interface aboutProps {}
-
-const about: React.FC<aboutProps> = () => {
-  return <Wrapper>about page!</Wrapper>;
+const about: React.FC<{}> = () => {
+  return (
+    <Wrapper>
+      <Flex height="500px" bg="gray.800" color="white" justify="center" py={10}>
+        <Box>WCG</Box>
+        <Box>image</Box>
+      </Flex>
+    </Wrapper>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async (props) => ({
