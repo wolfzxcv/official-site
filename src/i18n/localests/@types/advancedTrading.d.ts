7316a4758104b481d693a00ba85ec3b7d5107259
hwl244356 <2443569329@qq.com>
2021-09-18 17:47:00 +0800

    初级交易中级交易 --英文     IB代理语言

---
+++ src/i18n/localests/@types/advancedTrading.d.ts
@@ -0,0 +1,22 @@
+/* 高级交易课程 */
+export type AdvancedTrading={
+   /********
+    * advancedTrading
+   ********/
+  /* Analysis and trends */
+  analysisAndTrends:string;
+  /** Advanced Trading Course */
+  advancedTradingCourse: string;
+  /** Technical analysis */
+  technicalAnalysis: string;
+  /** Fundamental analysis */
+  fundamentalAnalysis: string;
+  /* Fibonacci Retracement Line */
+  fibonacciRetracementLine:string;
+  /** Support and resistance  */
+  supportAndResistance: string;
+  /** Market participants */
+  marketParticipants: string;
+  /** Trading style */
+  tradingStyle: string;
+}
---
+++ src/i18n/localests/en/advancedTrading.ts
@@ -0,0 +1,12 @@
+/* 高级交易课程 */
+import { AdvancedTrading } from "../@types/advancedTrading"
+export const advancedTrading:AdvancedTrading={
+    analysisAndTrends:'Analysis and trends',
+    advancedTradingCourse: 'Advanced*Trading*Course',
+    technicalAnalysis: 'Technical*analysis*and*market*trends',
+  fundamentalAnalysis: 'Fundamental*analysis',
+  fibonacciRetracementLine:'Fibonacci Retracement Line',
+  supportAndResistance: 'Support*and*resistance',
+  marketParticipants: 'Market*participants',
+  tradingStyle: 'Trading*style',
+}
