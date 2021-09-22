a64980585b49b6985ba56335aacdcab92c50ece7
Your Name <you@example.com>
2021-09-22 13:14:10 +0800

    完成了区域代表国际化

---
+++ src/i18n/localests/cn/regionalRepresentative.ts
@@ -0,0 +1,28 @@
+import { RegionalRepresentative } from '../@types/regionalRepresentative'
+export const regionalRepresentative:RegionalRepresentative={
+    regionalRepresentative: '区域代表',
+    thisIsAUnique: '对于想要在本地发展交易服务的人们来说，这是一个独一无二的机会。开设WCG区域办事处将是一个更高层次的合作选择，在您的地区管理自己的代理公司来获得稳定收益，同时以我们强大的优势为您提供行业内最好的交易条件及技术。',
+    representativeApplcation: '申请代表   >',
+    doNotMiss: '您的绝佳机会',
+    setYourOwn: '自由设定属于您自己的商业模式、营销蓝图、组织研讨会、网络研讨会以及对特定市场需求有益或相关的活动。我们将帮助您在您的当地市场获得稳定扎实的基础。',
+    brandsThatSahre: '共享全球信誉的品牌',
+    theSupportWe: '我们为区域办事处提供的支持包括但不仅限于满足发展本地业务的所有需求。WCG提供咨询服务、宣传数据和自己的预算，以及为更快更准确地提供支持而与客户直接地联络。我们为区域代表提供所有技术事项、实施和整合本机服务（例如当地最受欢迎的支付系统）的协助，以帮助优化他们的业务表现。',
+    soleRegional: '唯一地区代表',
+    youWillBe: '您将是您所在地区的唯一代表处，通过WCG的区域代表计划，您将享有该区的独家代理权。',
+    marketingAssistance: '行销协助支持',
+    provideComprehensive: '提供全面的技术支持，帮助优化及网络宣传，营销材料及帮助纪录并管理您的客户。',
+    competitiveEdge: '享有特殊待遇',
+    weWillProvide: '我们将为您的客户提供具有竞争力的交易条件，迅速提升您在该区域的交易优势。',
+    regionalRepresentativeIsIncome: '区域代表的收入',
+    yourIncomeIncludes: '您的收入包含了来自每一笔推荐人交易的标准佣金，以及来自下线账户的收入，以及额外的代',
+    theSpecificAmount: '理酬劳，额外收入的具体额度可与合伙人共同商定后决定。',
+    become: '成为',
+    yourOwnBoss: '自己的老板',
+    become1: '成为',
+    ourPartner: '我们的伙伴',
+    get: '获得',
+    technicalSupport: '技术的支持',
+    becomeOurRegional: '成为我们的区域代表',
+    wCGIsCurrently: 'WCG目前正在快速发展中，这意味着我们将不断寻求并且开发新的市场，而我们的客户正在成为我们商业模式其中一部分来与我们共同成长。',
+    applyNow: '立即申请   >'
+}
