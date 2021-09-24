41db736fede3df1df6cf563f4cfe10b562ccdb64
Alice Chou <alice1qaz@gmail.com>
2021-09-24 10:02:33 +0800

    style(i18n): modify iBBroker, regionalRepresentative, whiteLabelCooperation

--- src/i18n/localests/vi/regionalRepresentative.ts
+++ src/i18n/localests/vi/regionalRepresentative.ts
@@ -1,28 +1,37 @@
-import { RegionalRepresentative } from '../@types/regionalRepresentative'
-export const regionalRepresentative:RegionalRepresentative={
-    regionalRepresentative: 'Đại*diện*khu*vực',
-    thisIsAUnique: 'Đây*là*một*cơ*hội*duy*nhất*cho*những*người*muốn*phát*triển*doanh*nghiệp*tại*địa*phương.*Mở*văn*phòng*khu*vực*WCG*sẽ*là*một*lựa*chọn*hợp*tác*cấp*cao*hơn.*Quản*lý*công*ty*đại*lý*của*riêng*bạn*tại*địa*phương*để*có*thu*nhập*ổn*định,*đồng*thời*cung*cấp*cho*bạn*các*điều*kiện*giao*dịch*và*công*nghệ*tốt*nhất*trong*ngành*với*lợi*thế*cạnh*tranh*của*chúng*tôi.',
-    representativeApplcation: 'Ứng*dụng*đại*diện***>',
-    doNotMiss: 'Đừng*bỏ*lỡ*cơ*hội*này!',
-    setYourOwn: 'Tự*do*thiết*lập*mô*hình*kinh*doanh*của*riêng*bạn,*kế*hoạch*tiếp*thị*của*bạn,*tổ*chức*hội*thảo,*hội*thảo*trên*web*và*các*hoạt*động*có*lợi*hoặc*liên*quan*đến*nhu*cầu*thị*trường*cụ*thể.*Chúng*tôi*sẽ*hỗ*trợ*bạn*có*được*nền*tảng*ổn*định*và*vững*chắc*tại*thị*trường*địa*phương*của*bạn.',
-    brandsThatSahre: 'Các*thương*hiệu*có*danh*tiếng*toàn*cầu',
-    theSupportWe: 'Sự*hỗ*trợ*mà*chúng*tôi*cung*cấp*cho*các*văn*phòng*khu*vực*bao*gồm*nhưng*không*giới*hạn*để*đáp*ứng*tất*cả*các*nhu*cầu*phát*triển*doanh*nghiệp*địa*phương.*WCG*cung*cấp*dịch*vụ*tư*vấn,*tài*liệu*quảng*cáo*và*ngân*sách*tiếp*thị,*cũng*như*liên*hệ*trực*tiếp*với*khách*hàng*để*được*hỗ*trợ*nhanh*chóng*và*chính*xác*hơn.*Chúng*tôi*cung*cấp*hỗ*trợ*cho*các*đại*diện*khu*vực*trong*tất*cả*các*vấn*đề*kỹ*thuật,*triển*khai*và*tích*hợp*các*dịch*vụ*địa*phương*(chẳng*hạn*như*hệ*thống*thanh*toán*phổ*biến*nhất*trong*khu*vực)*để*giúp*tối*ưu*hóa*hiệu*quả*kinh*doanh*của*họ.',
-    soleRegional: 'Đại*diện*khu*vực*duy*nhất',
-    youWillBe: 'Bạn*sẽ*là*đại*diện*duy*nhất*trong*khu*vực*của*bạn.*Thông*qua*chương*trình*đại*diện*khu*vực*của*WCG,*bạn*sẽ*được*hưởng*quyền*đại*lý*độc*quyền*trong*khu*vực*cụ*thể*này.',
-    marketingAssistance: 'Hỗ*trợ*hỗ*trợ*tiếp*thị',
-    provideComprehensive: 'Cung*cấp*hỗ*trợ*kỹ*thuật*toàn*diện*để*giúp*tối*ưu*hóa*việc*quảng*bá*mạng,*tài*liệu*tiếp*thị*và*giúp*ghi*lại*và*quản*lý*khách*hàng*của*bạn.',
-    competitiveEdge: 'Lợi*thế*cạnh*tranh',
-    weWillProvide: 'Chúng*tôi*sẽ*cung*cấp*cho*khách*hàng*của*bạn*các*điều*kiện*giao*dịch*cạnh*tranh*và*nhanh*chóng*nâng*cao*lợi*thế*giao*dịch*của*bạn*trong*khu*vực*của*bạn.',
-    regionalRepresentativeIsIncome: 'Thu*nhập*của*đại*diện*khu*vực',
-    yourIncomeIncludes: 'Thu*nhập*của*bạn*bao*gồm*hoa*hồng*tiêu*chuẩn*từ*mỗi*giao*dịch*của*người*giới*thiệu,*cũng*như*thu*nhập*từ*tất*cả*các*tài*khoản*tuyến*dưới*của*bạn*và*thù*lao*đại*lý*bổ*sung.*Mức*thu*nhập',
-    theSpecificAmount: 'bổ*sung*cụ*thể*có*thể*được*xác*định*sau*khi*thỏa*thuận*với*đối*tác.*',
-    become: 'Tự*làm',
-    yourOwnBoss: 'chủ*chính*mình',
-    become1: 'Trở*thành',
-    ourPartner: 'đối*tác*của*chúng*tôi',
-    get: 'Nhận',
-    technicalSupport: 'hỗ*trợ*kỹ*thuật',
-    becomeOurRegional: 'Trở*thành*đại*diện*khu*vực*của*chúng*tôi',
-    wCGIsCurrently: 'WCG*hiện*đang*trong*quá*trình*phát*triển*nhanh*chóng,*có*nghĩa*là*chúng*tôi*sẽ*tiếp*tục*tìm*kiếm*và*phát*triển*các*thị*trường*mới,*và*khách*hàng*của*chúng*tôi*đang*trở*thành*một*phần*trong*mô*hình*kinh*doanh*để*cùng*chúng*tôi*phát*triển.',
-    applyNow: 'Đăng*ký*ngay***>'
-}
+import { RegionalRepresentative } from '../@types/regionalRepresentative';
+export const regionalRepresentative: RegionalRepresentative = {
+  regionalRepresentative: 'Đại*diện*khu*vực',
+  thisIsAUnique:
+    'Đây*là*một*cơ*hội*duy*nhất*cho*những*người*muốn*phát*triển*doanh*nghiệp*tại*địa*phương.*Mở*văn*phòng*khu*vực*WCG*sẽ*là*một*lựa*chọn*hợp*tác*cấp*cao*hơn.*Quản*lý*công*ty*đại*lý*của*riêng*bạn*tại*địa*phương*để*có*thu*nhập*ổn*định,*đồng*thời*cung*cấp*cho*bạn*các*điều*kiện*giao*dịch*và*công*nghệ*tốt*nhất*trong*ngành*với*lợi*thế*cạnh*tranh*của*chúng*tôi.',
+  representativeApplcation: 'Ứng*dụng*đại*diện',
+  doNotMiss: 'Đừng*bỏ*lỡ*cơ*hội*này!',
+  setYourOwn:
+    'Tự*do*thiết*lập*mô*hình*kinh*doanh*của*riêng*bạn,*kế*hoạch*tiếp*thị*của*bạn,*tổ*chức*hội*thảo,*hội*thảo*trên*web*và*các*hoạt*động*có*lợi*hoặc*liên*quan*đến*nhu*cầu*thị*trường*cụ*thể.*Chúng*tôi*sẽ*hỗ*trợ*bạn*có*được*nền*tảng*ổn*định*và*vững*chắc*tại*thị*trường*địa*phương*của*bạn.',
+  brandsThatSahre: 'Các*thương*hiệu*có*danh*tiếng*toàn*cầu',
+  theSupportWe:
+    'Sự*hỗ*trợ*mà*chúng*tôi*cung*cấp*cho*các*văn*phòng*khu*vực*bao*gồm*nhưng*không*giới*hạn*để*đáp*ứng*tất*cả*các*nhu*cầu*phát*triển*doanh*nghiệp*địa*phương.*WCG*cung*cấp*dịch*vụ*tư*vấn,*tài*liệu*quảng*cáo*và*ngân*sách*tiếp*thị,*cũng*như*liên*hệ*trực*tiếp*với*khách*hàng*để*được*hỗ*trợ*nhanh*chóng*và*chính*xác*hơn.*Chúng*tôi*cung*cấp*hỗ*trợ*cho*các*đại*diện*khu*vực*trong*tất*cả*các*vấn*đề*kỹ*thuật,*triển*khai*và*tích*hợp*các*dịch*vụ*địa*phương*(chẳng*hạn*như*hệ*thống*thanh*toán*phổ*biến*nhất*trong*khu*vực)*để*giúp*tối*ưu*hóa*hiệu*quả*kinh*doanh*của*họ.',
+  soleRegional: 'Đại*diện*khu*vực*duy*nhất',
+  youWillBe:
+    'Bạn*sẽ*là*đại*diện*duy*nhất*trong*khu*vực*của*bạn.*Thông*qua*chương*trình*đại*diện*khu*vực*của*WCG,*bạn*sẽ*được*hưởng*quyền*đại*lý*độc*quyền*trong*khu*vực*cụ*thể*này.',
+  marketingAssistance: 'Hỗ*trợ*hỗ*trợ*tiếp*thị',
+  provideComprehensive:
+    'Cung*cấp*hỗ*trợ*kỹ*thuật*toàn*diện*để*giúp*tối*ưu*hóa*việc*quảng*bá*mạng,*tài*liệu*tiếp*thị*và*giúp*ghi*lại*và*quản*lý*khách*hàng*của*bạn.',
+  competitiveEdge: 'Lợi*thế*cạnh*tranh',
+  weWillProvide:
+    'Chúng*tôi*sẽ*cung*cấp*cho*khách*hàng*của*bạn*các*điều*kiện*giao*dịch*cạnh*tranh*và*nhanh*chóng*nâng*cao*lợi*thế*giao*dịch*của*bạn*trong*khu*vực*của*bạn.',
+  regionalRepresentativeIsIncome: 'Thu*nhập*của*đại*diện*khu*vực',
+  yourIncomeIncludes:
+    'Thu*nhập*của*bạn*bao*gồm*hoa*hồng*tiêu*chuẩn*từ*mỗi*giao*dịch*của*người*giới*thiệu,*cũng*như*thu*nhập*từ*tất*cả*các*tài*khoản*tuyến*dưới*của*bạn*và*thù*lao*đại*lý*bổ*sung.*Mức*thu*nhập',
+  theSpecificAmount:
+    'bổ*sung*cụ*thể*có*thể*được*xác*định*sau*khi*thỏa*thuận*với*đối*tác.*',
+  become: 'Tự*làm',
+  yourOwnBoss: 'chủ*chính*mình',
+  become1: 'Trở*thành',
+  ourPartner: 'đối*tác*của*chúng*tôi',
+  get: 'Nhận',
+  technicalSupport: 'hỗ*trợ*kỹ*thuật',
+  becomeOurRegional: 'Trở*thành*đại*diện*khu*vực*của*chúng*tôi',
+  wCGIsCurrently:
+    'WCG*hiện*đang*trong*quá*trình*phát*triển*nhanh*chóng,*có*nghĩa*là*chúng*tôi*sẽ*tiếp*tục*tìm*kiếm*và*phát*triển*các*thị*trường*mới,*và*khách*hàng*của*chúng*tôi*đang*trở*thành*một*phần*trong*mô*hình*kinh*doanh*để*cùng*chúng*tôi*phát*triển.',
+  applyNow: 'Đăng*ký*ngay'
+};
--- src/pages/about/companyNews.tsx
+++ src/pages/about/companyNews.tsx
@@ -1,10 +1,33 @@
+import axios from 'axios';
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import Wrapper from '../../components/Wrapper';
+import { Locales } from '../../i18n/locales';
 
 const companyNews: React.FC<{}> = () => {
-  return <Wrapper>companyNews page!</Wrapper>;
+  const router = useRouter();
+  const currentLang = router.locale as Locales;
+
+  const [info, setInfo] = useState([]);
+  useEffect(() => {
+    loadData(currentLang);
+  }, []);
+
+  const loadData = async (currentLang) => {
+    const {
+      data: {
+        data: { com }
+      }
+    } = await axios.get(
+      `http://www.wc012.com/api/index/company?lang=${currentLang}`
+    );
+    console.log(com);
+    setInfo(com);
+  };
+
+  return <Wrapper>companyNews page! </Wrapper>;
 };
 
 export const getStaticProps: GetStaticProps = async (props) => ({
