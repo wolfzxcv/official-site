ea56935f37397117e1218963e8f86f4bbf8dc94a
Alice <alice1qaz@gmail.com>
2021-10-08 16:22:05 +0800

    fix(apidatalist): add link button when needed

--- src/components/Common/ApiDataList.tsx
+++ src/components/Common/ApiDataList.tsx
@@ -23,6 +23,28 @@ const ApiDataList: React.FC<ApiDataListProps> = ({
 
   const [items, setItems] = useState([]);
 
+  let buttonText;
+  switch (currentLang) {
+    case 'cn':
+      buttonText = '查看详情';
+      break;
+    case 'id':
+      buttonText = 'Silakan mengunjungi';
+      break;
+    case 'ms':
+      buttonText = 'Sila melawat';
+      break;
+    case 'vi':
+      buttonText = 'Xin hãy đến';
+      break;
+    case 'zh':
+      buttonText = '查看詳情';
+      break;
+    default:
+      buttonText = 'View';
+      break;
+  }
+
   const getItems = async (currentLang) => {
     try {
       const {
@@ -37,7 +59,11 @@ const ApiDataList: React.FC<ApiDataListProps> = ({
               id: x.id,
               date: x.time && x.time.length > 10 ? x.time.slice(0, 10) : x.time,
               title: x.title,
-              htmlContent: x.content
+              htmlContent:
+                x.content +
+                (x.out_url
+                  ? `<br/><br/><a target="_blank" style="background-color: #C53030; color: white; padding: 10px;" href="${x.out_url}">${buttonText}  ></a>`
+                  : '')
             }))
           : [];
 
