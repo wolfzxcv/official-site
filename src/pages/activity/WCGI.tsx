89cb4610c1cc7d02728c129bdb7624c41867ba4f
Alice <alice1qaz@gmail.com>
2021-11-02 16:31:39 +0800

    style(wcgi): wCGI page kind of finished

--- src/pages/activity/WCGI.tsx
+++ src/pages/activity/WCGI.tsx
@@ -4,30 +4,130 @@ import LiveChat from '../../components/LiveChat/LiveChat';
 import { openChatWindow } from '../../utils';
 
 const white = 'white';
+const orange = '#f55648';
+const red = '#a60203';
 const darkRed = '#620000';
 
 const WCGI: React.FC<{}> = () => {
   return (
     <>
-      {/* PC layout */}
+      <Box width="100%" bg={red}>
+        {/* PC layout 1280px up */}
+        <Box
+          display={{ base: 'none', xl: 'block' }}
+          bgImage="../assets/images/activity_WCGI_bg.jpg"
+          bgPosition="center top"
+          bgRepeat="no-repeat"
+          height="3380px"
+        >
+          <TopLinks />
+
+          {/*  立即開戶 */}
+          <Flex
+            position="absolute"
+            top="930px"
+            justify="space-evenly"
+            width="100%"
+          >
+            <Box></Box>
+            <Box ml={20}>
+              <Link
+                _hover={{
+                  textDecoration: 'none'
+                }}
+                href="https://trader.wc012.com/register"
+                isExternal
+              >
+                <Image
+                  height="auto"
+                  src="../assets/images/activity_WCGI_buttonb.png"
+                  alt="b"
+                />
+              </Link>
+            </Box>
+          </Flex>
+
+          {/* 在線諮詢 + 立即開戶 */}
+          <Flex position="absolute" top="2500px" justify="center" width="100%">
+            <Box mr={5}>
+              <Image
+                onClick={openChatWindow}
+                _hover={{
+                  cursor: 'pointer'
+                }}
+                src="../assets/images/activity_WCGI_buttona.png"
+                alt="a"
+              />
+            </Box>
+
+            <Box>
+              <Link
+                _hover={{
+                  textDecoration: 'none'
+                }}
+                href="https://trader.wc012.com/register"
+                isExternal
+              >
+                <Image
+                  src="../assets/images/activity_WCGI_buttonb.png"
+                  alt="b"
+                />
+              </Link>
+            </Box>
+          </Flex>
+        </Box>
+      </Box>
+
+      {/* Mobile layout */}
       <Box
-        display={{ base: 'none', xl: 'block' }}
-        bgImage="../assets/images/activity_WCGI_bg2.jpg"
-        bgPosition="center top"
-        bgRepeat="no-repeat"
-        height="3380px"
+        width="100%"
+        bg={red}
+        pb={20}
+        display={{ base: 'block', xl: 'none' }}
       >
         <TopLinks />
 
-        {/*  立即開戶 */}
-        <Flex
-          position="absolute"
-          top="930px"
-          justify="space-evenly"
-          width="100%"
-        >
-          <Box></Box>
-          <Box ml={20}>
+        <Image
+          mx="auto"
+          height="auto"
+          src="../assets/images/activity_WCGI_banner_word.png"
+          alt="banner_word"
+        />
+        <Flex bg={red} padding={10} flexDirection="column" align="center">
+          <Box
+            width="100%"
+            maxW="680px"
+            bg={white}
+            color={orange}
+            border="4px"
+            borderColor={orange}
+            borderRadius="20px"
+            paddingX={{ base: '1rem', md: 10 }}
+            paddingY={5}
+            fontSize={{ base: '10px', md: '18px' }}
+            fontWeight="600"
+          >
+            活动期间内，特选客户成功1,000美元或以上即可获得活动体验资格，符合资格客户完成指定交易手数可免费获得以下苹果系列产品一份。
+          </Box>
+
+          <Box
+            width="100%"
+            maxW="680px"
+            mt={10}
+            bg={red}
+            color={white}
+            border="4px dashed"
+            borderColor={white}
+            paddingX={{ base: '1rem', md: 10 }}
+            paddingY={5}
+            fontSize={{ base: '10px', md: '18px' }}
+            fontWeight="600"
+          >
+            <Text>活动开始日期：2021年11月1日 — 2022年12月31日</Text>
+            <Text>活动对象：特选客户及新客户 </Text>
+          </Box>
+
+          <Box my={20}>
             <Link
               _hover={{
                 textDecoration: 'none'
@@ -37,43 +137,160 @@ const WCGI: React.FC<{}> = () => {
             >
               <Image
                 height="auto"
-                src="../assets/images/activity_JYSL_buttonb.png"
+                src="../assets/images/activity_WCGI_buttonb.png"
                 alt="b"
               />
             </Link>
           </Box>
+
+          <Box
+            width="100%"
+            maxW="750px"
+            color={white}
+            paddingX={{ base: '1rem', md: 10 }}
+            paddingY={5}
+            fontSize={{ base: '10px', md: '18px' }}
+            fontWeight="600"
+          >
+            <Text>活动规则： </Text>
+            <Text>
+              １）在活动期间内成功入金1,000美元或以上即可获得参加活动资格，活动结束前完成指定交易手数可免费获得苹果系列产品一份。
+            </Text>
+            <Text>２）参加本次优惠活动的交易产品仅限贵金属及原油。</Text>
+          </Box>
         </Flex>
 
-        {/* 在線諮詢 + 立即開戶 */}
-        <Flex position="absolute" top="2500px" justify="center" width="100%">
-          <Box mr={5}>
+        <Center>
+          <Flex
+            bg={white}
+            py={5}
+            width={{ base: '85vw', md: '800px' }}
+            justify="space-evenly"
+            align="center"
+          >
             <Image
+              width="40%"
+              height="auto"
+              src="../assets/images/activity_WCGI_2500.png"
+              alt="15"
+            />
+            <Image
+              width="40%"
+              height="auto"
+              src="../assets/images/activity_WCGI_2000.png"
+              alt="5"
+            />
+          </Flex>
+        </Center>
+
+        <Center>
+          <Flex
+            bg={white}
+            py={5}
+            width={{ base: '85vw', md: '800px' }}
+            justify="space-evenly"
+            align="center"
+          >
+            <Image
+              width="40%"
+              height="auto"
+              src="../assets/images/activity_WCGI_1500.png"
+              alt="15"
+            />
+            <Image
+              width="40%"
+              height="auto"
+              src="../assets/images/activity_WCGI_1000.png"
+              alt="5"
+            />
+          </Flex>
+        </Center>
+
+        <Center>
+          <Flex
+            bg={white}
+            p={10}
+            width={{ base: '85vw', md: '800px' }}
+            justify="space-evenly"
+            align="center"
+          >
+            <Box
+              bg={red}
+              color={white}
+              p={10}
+              border="4px"
+              borderRadius="10px"
+              borderColor="orange"
+            >
+              例子：客户陈先生在活动期间内成功注资2,500美元，即可获得参加活动资格。陈先生在此次活动中完成了100手黄金及100手原油交易，于活动结束后可以免费获得以下苹果系列产品iPhone
+              13 Pro手机一份。
+            </Box>
+          </Flex>
+        </Center>
+
+        <Center>
+          <Flex
+            bg={white}
+            width={{ base: '85vw', md: '800px' }}
+            px={{ base: 5, md: 20 }}
+            py={10}
+            justify="space-evenly"
+            align="center"
+            wrap="wrap"
+          >
+            <Image
+              width={{ base: '80%', md: '50%' }}
+              pr={{ base: 0, md: 2 }}
+              height="auto"
               onClick={openChatWindow}
               _hover={{
                 cursor: 'pointer'
               }}
-              src="../assets/images/activity_JYSL_buttona.png"
+              src="../assets/images/activity_WCGI_buttona.png"
               alt="a"
             />
-          </Box>
-
-          <Box>
             <Link
               _hover={{
                 textDecoration: 'none'
               }}
               href="https://trader.wc012.com/register"
               isExternal
+              width={{ base: '80%', md: '50%' }}
+              pt={{ base: 5, md: 0 }}
+              pl={{ base: 0, md: 2 }}
             >
-              <Image src="../assets/images/activity_JYSL_buttonb.png" alt="b" />
+              <Image
+                height="auto"
+                width="100%"
+                src="../assets/images/activity_WCGI_buttonb.png"
+                alt="b"
+              />
             </Link>
-          </Box>
-        </Flex>
-      </Box>
+          </Flex>
+        </Center>
 
-      {/* Mobile layout */}
-      <Box display={{ base: 'block', xl: 'none' }}>
-        <TopLinks />
+        <Center>
+          {/* 加入公众号 */}
+          <Flex bg={white} justify="center">
+            <Flex
+              width={{ base: '85vw', md: '800px' }}
+              px={{ base: 5, md: 20 }}
+              py={10}
+              flexDirection="column"
+              justify="center"
+              align="center"
+            >
+              <Box pt={5}>
+                <Image
+                  height="auto"
+                  width="100%"
+                  src="../assets/images/activity_WCGI_GZH_LOGO.png"
+                  alt="GZH_LOGO"
+                />
+              </Box>
+            </Flex>
+          </Flex>
+        </Center>
       </Box>
 
       {/* 注意事项 */}
@@ -183,7 +400,7 @@ const TopLinks = () => {
                 display="inline"
                 height="inherit"
                 width="auto"
-                src="../assets/images/activity_JYSL_icon_1.png"
+                src="../assets/images/activity_WCGI_icon_1.png"
                 alt="icon_1"
               />
               官网首页
@@ -201,7 +418,7 @@ const TopLinks = () => {
               display="inline"
               height="inherit"
               width="auto"
-              src="../assets/images/activity_JYSL_icon_2.png"
+              src="../assets/images/activity_WCGI_icon_2.png"
               alt="icon_2"
             />
             在线客服
