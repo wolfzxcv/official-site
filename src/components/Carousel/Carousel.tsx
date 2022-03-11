376976595db1c3819c502877525a461a05b929cc
Alice <alice1qaz@gmail.com>
2022-03-11 15:02:21 +0800

    refactor(carousel): carousel add swipe

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,6 +1,16 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
-import { Box, Fade, Flex, Icon } from '@chakra-ui/react';
+import { Box, Fade, Flex, Icon, keyframes } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
+
+const arrowsPositionFromTheTop = '500px';
+
+const barPositionFromTheTop = '600px';
+
+const arrowColor = 'blue.700';
+
+const unselectedItem = 'blue.700';
+
+const selectedItem = 'red.500';
 
 type CarouselProps = {
   defaultSlider: JSX.Element;
@@ -8,18 +18,24 @@ type CarouselProps = {
   showArrow?: boolean;
   duration?: number;
   transition?: number;
+  type?: 'circle' | 'bar';
 };
 
 const Carousel: React.FC<CarouselProps> = ({
   defaultSlider,
   sliders,
-  showArrow = false,
-  duration = 1,
-  transition = 10
+  showArrow = true,
+  duration = 0.5,
+  transition = 10,
+  type = 'circle'
 }: CarouselProps) => {
   const [index, setIndex] = useState(0);
 
   const [isPlay, setIsPlay] = useState(true);
+
+  const defaultClientXValue = 0;
+
+  const [clientXValue, setClientXValue] = useState(defaultClientXValue);
 
   const allSliders = sliders && sliders.length > 0 ? sliders : [defaultSlider];
 
@@ -69,19 +85,53 @@ const Carousel: React.FC<CarouselProps> = ({
     handleSelect(result);
   };
 
+  const handleMove = async (value: number, type: 'start' | 'end') => {
+    if (type === 'start') {
+      await setClientXValue(value);
+    } else if (type === 'end') {
+      const startValue = clientXValue;
+      const endValue = value;
+
+      if (startValue > endValue) {
+        // start value > end value : left swipe, so equal to right arrow click
+        handleLeftArrowClick();
+      } else if (startValue < endValue) {
+        // start value < end value : right swipe, so equal to left arrow click
+        handleRightArrowClick();
+      }
+
+      // reset start value
+      await setClientXValue(0);
+    }
+  };
+
   return (
-    <Box minH="500px">
-      <Fade key={index} in={true} transition={{ enter: { duration } }}>
+    <Box minH={arrowsPositionFromTheTop}>
+      <Fade
+        key={index}
+        in={true}
+        transition={{ enter: { duration } }}
+        onTouchStart={(e: React.TouchEvent) =>
+          handleMove(e.changedTouches[0].clientX, 'start')
+        }
+        onTouchMove={(e: React.TouchEvent) => e.preventDefault()}
+        onTouchEnd={(e: React.TouchEvent) =>
+          handleMove(e.changedTouches[0].clientX, 'end')
+        }
+        onMouseDown={(e: React.MouseEvent) => handleMove(e.clientX, 'start')}
+        onMouseUp={(e: React.MouseEvent) => handleMove(e.clientX, 'end')}
+      >
         {allSliders[index]}
         {showArrow && (
           <ChevronLeftIcon
+            display={{ base: 'none', md: showArrow ? 'block' : 'none' }}
             zIndex={5}
             position="absolute"
             left={{ base: 0, md: 0 }}
-            top="500px"
+            top={arrowsPositionFromTheTop}
             onClick={() => handleLeftArrowClick()}
             fontSize={{ base: '72px', md: '96px' }}
-            color="blue.700"
+            color={arrowColor}
             bg="transparent"
             borderRadius="50%"
             _hover={{ cursor: 'pointer' }}
@@ -89,32 +139,90 @@ const Carousel: React.FC<CarouselProps> = ({
         )}
         {showArrow && (
           <ChevronRightIcon
+            display={{ base: 'none', md: showArrow ? 'block' : 'none' }}
             zIndex={5}
             position="absolute"
             right={{ base: 0, md: 20 }}
-            top="500px"
+            top={arrowsPositionFromTheTop}
             onClick={() => handleRightArrowClick()}
             _hover={{ cursor: 'pointer' }}
             fontSize={{ base: '72px', md: '96px' }}
-            color="blue.700"
+            color={arrowColor}
             bg="transparent"
             borderRadius="50%"
           />
         )}
-      </Fade>
-      <Flex justify="center" mt={5}>
-        {allSliders.map((each, idx) => (
+        {type === 'bar' && (
           <Flex
-            key={idx}
-            onClick={() => handleSelect(idx)}
-            align="center"
-            _hover={{ cursor: 'pointer' }}
+            zIndex={5}
+            top={barPositionFromTheTop}
+            position="absolute"
+            bg="transparent"
+            width="100%"
           >
-            <Dot dotIndex={idx} currentIndex={index} />
+            <Flex mx={'auto'}>
+              {allSliders.map((each, idx) => (
+                <Flex
+                  key={idx}
+                  onClick={() => handleSelect(idx)}
+                  align="center"
+                  _hover={{ cursor: 'pointer' }}
+                >
+                  <ProgressBar
+                    dotIndex={idx}
+                    currentIndex={index}
+                    transition={transition}
+                  />
+                </Flex>
+              ))}
+            </Flex>
           </Flex>
-        ))}
-      </Flex>
+        )}
+      </Fade>
+      {type === 'circle' && (
+        <Flex justify="center" mt={5}>
+          {allSliders.map((each, idx) => (
+            <Flex
+              key={idx}
+              onClick={() => handleSelect(idx)}
+              align="center"
+              _hover={{ cursor: 'pointer' }}
+            >
+              <Dot dotIndex={idx} currentIndex={index} />
+            </Flex>
+          ))}
+        </Flex>
+      )}
     </Box>
+  );
+};
+
+const widthChange = keyframes`
+0% {
+    width:0%;
+    
+  }
+  100% {
+    width: 100%;   
+  }
+`;
+
+type ProgressBarProps = DotProps & { transition: number };
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  dotIndex,
+  currentIndex,
+  transition
+}: ProgressBarProps) => {
+  const isSelected = dotIndex === currentIndex;
+  return (
+    <Flex width="50px" height="8px" bg={unselectedItem} mx={2} justify="start">
+      <Box
+        animation={`${widthChange} ${transition}s linear infinite`}
+        height="8px"
+        bg={isSelected ? selectedItem : 'auto'}
+      ></Box>
+    </Flex>
   );
 };
 
@@ -123,13 +231,16 @@ type DotProps = {
   currentIndex: number;
 };
 
-const Dot: React.FC<DotProps> = ({ dotIndex, currentIndex }: DotProps) => {
+export const Dot: React.FC<DotProps> = ({
+  dotIndex,
+  currentIndex
+}: DotProps) => {
   const isSelected = dotIndex === currentIndex;
 
   return (
     <CircleIcon
       boxSize={isSelected ? 6 : 8}
-      color={isSelected ? 'red.500' : 'blue.700'}
+      color={isSelected ? selectedItem : unselectedItem}
     />
   );
 };
