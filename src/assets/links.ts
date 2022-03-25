039cafa6cc3a3c59de634f0fa8508ec50739e47c
Alice Chou <alice1qaz@gmail.com>
2022-03-25 11:30:08 +0800

    refactor(links): modify links

--- src/assets/links.ts
+++ src/assets/links.ts
@@ -4,6 +4,8 @@ export const links = {
   telegram: 'https://t.me/WCGMarketsLtd',
   facebook: 'https://www.facebook.com/WCG-109473260957040',
   instagram: 'https://www.instagram.com/wcg_markets',
+  weibo: 'https://m.weibo.cn/u/7727884823',
+  wechat: 'http://weixin.qq.com/r/eRIzKz3E0mCtrXlI90dr',
   mt4Android:
     'https://play.google.com/store/apps/details?id=net.metaquotes.metatrader4&hl=en&referrer=ref_id%3d5189084203383832573%26server%3dWenChuanGlobalLtd-Demo%252cWenChuanGlobalLtd-Live',
   mt4IOS: 'https://apps.apple.com/us/app/metatrader-4/id496212596',
@@ -18,5 +20,7 @@ export const links = {
   // 代理中心
   iBCenter: 'https://my.wcgmarkets.com/',
   // 聯絡信箱
-  email: 'cs@wcgmarkets.com'
+  email: 'cs@wcgmarkets.com',
+  // 聯絡電話
+  tel: '4001203612'
 };
