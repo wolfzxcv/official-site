41c2aa2014bef2af1a60f07760eba13edaf06f9a
Alice Chou <alice1qaz@gmail.com>
2024-05-28 20:17:27 +0200

    refactor(apidatalist): modify type ApiDataListProps; remove unused component

--- src/components/Common/ApiDataList.tsx
+++ src/components/Common/ApiDataList.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
 type ApiDataListProps = {
-  api: '/market' | '/responsibility' | '/notice' | '/news' | '/checkip';
+  api: '/market' | '/responsibility' | '/notice' | '/news';
 };
 
 const InfoAccordion = dynamic(() => import('./InfoAccordion'), { ssr: false });
